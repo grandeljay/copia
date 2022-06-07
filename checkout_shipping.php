315dd28d76b4fe03010915fb44b5ee571143dcc1
grandeljay <github.jay@grandel.anonaddy.me>
2022-06-07 09:22:28 +0200

    Update checkout_shipping.php

--- checkout_shipping.php
+++ checkout_shipping.php
@@ -27,7 +27,7 @@
 
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
-include('includes/application_top.php');
+include 'includes/application_top.php';
 
 // pre-selection the cheapest shipping option
 defined('CHECK_CHEAPEST_SHIPPING_MODUL') or define('CHECK_CHEAPEST_SHIPPING_MODUL', 'false'); // default: 'false'
@@ -39,11 +39,11 @@ defined('SHOW_SELFPICKUP_FREE') or define('SHOW_SELFPICKUP_FREE', 'false'); // d
 $smarty = new Smarty();
 
 // include needed functions
-require_once(DIR_FS_INC . 'xtc_address_label.inc.php');
-require_once(DIR_FS_INC . 'xtc_get_address_format_id.inc.php');
-require_once(DIR_FS_INC . 'xtc_count_shipping_modules.inc.php');
+require_once DIR_FS_INC . 'xtc_address_label.inc.php';
+require_once DIR_FS_INC . 'xtc_get_address_format_id.inc.php';
+require_once DIR_FS_INC . 'xtc_count_shipping_modules.inc.php';
 
-require(DIR_WS_INCLUDES . 'checkout_requirements.php');
+require DIR_WS_INCLUDES . 'checkout_requirements.php';
 
 unset($_SESSION['tmp_oID']);
 unset($_SESSION['paypal']);
@@ -51,10 +51,12 @@ unset($_SESSION['paypal']);
 //express checkout
 if (defined('MODULE_CHECKOUT_EXPRESS_STATUS') && MODULE_CHECKOUT_EXPRESS_STATUS == 'true') {
     if (isset($_GET['express']) && $_GET['express'] == 'on') {
-        $express_query = xtc_db_query("SELECT checkout_shipping,
+        $express_query = xtc_db_query(
+            "SELECT checkout_shipping,
                                           checkout_shipping_address
                                      FROM " . TABLE_CUSTOMERS_CHECKOUT . "
-                                    WHERE customers_id = '" . (int)$_SESSION['customer_id'] . "'");
+                                    WHERE customers_id = '" . (int)$_SESSION['customer_id'] . "'"
+        );
         if (xtc_db_num_rows($express_query) > 0) {
             $express = xtc_db_fetch_array($express_query);
             if ($express['checkout_shipping_address'] != '') {
@@ -69,12 +71,14 @@ if (!isset($_SESSION['sendto'])) {
     $_SESSION['sendto'] = $_SESSION['customer_default_address_id'];
 } else {
     // verify the selected shipping address
-    $check_address_query = xtc_db_query("SELECT count(*) as total
-	                                       FROM " . TABLE_ADDRESS_BOOK . "
-	                                      WHERE customers_id = '" . (int) $_SESSION['customer_id'] . "'
-	                                        AND address_book_id = '" . (int) $_SESSION['sendto'] . "'");
-    $check_address = xtc_db_fetch_array($check_address_query);
-    if ($check_address['total'] != '1') {
+    $check_address_query = xtc_db_query(
+        "SELECT count(*) as total
+	       FROM " . TABLE_ADDRESS_BOOK . "
+	      WHERE customers_id = '" . (int) $_SESSION['customer_id'] . "'
+	        AND address_book_id = '" . (int) $_SESSION['sendto'] . "'"
+    );
+    $check_address       = xtc_db_fetch_array($check_address_query);
+    if ('1' != $check_address['total']) {
         $_SESSION['sendto'] = $_SESSION['customer_default_address_id'];
         if (isset($_SESSION['shipping'])) {
             unset($_SESSION['shipping']);
@@ -86,7 +90,7 @@ if (!isset($_SESSION['billto'])) {
     $_SESSION['billto'] = $_SESSION['customer_default_address_id'];
 }
 
-require_once(DIR_WS_CLASSES . 'order.php');
+require_once DIR_WS_CLASSES . 'order.php';
 $order = new order();
 
 // avoid hack attempts during the checkout procedure by checking the internal cartID
@@ -105,24 +109,24 @@ $_SESSION['cartID'] = $_SESSION['cart']->cartID;
 // a shipping address is not needed
 if ($order->content_type == 'virtual' || ($order->content_type == 'virtual_weight') || ($_SESSION['cart']->count_contents_virtual() == 0)) { // GV Code added
     $_SESSION['shipping'] = false;
-    $_SESSION['sendto'] = false;
+    $_SESSION['sendto']   = false;
     xtc_redirect(xtc_href_link(FILENAME_CHECKOUT_PAYMENT, xtc_get_all_get_params(), 'SSL'));
 }
 
 $total_weight = $_SESSION['cart']->show_weight();
-$total_count = $_SESSION['cart']->count_contents();
+$total_count  = $_SESSION['cart']->count_contents();
 
 if ($order->delivery['country']['iso_code_2'] != '') {
     $_SESSION['delivery_zone'] = $order->delivery['country']['iso_code_2'];
 }
 
 // load all enabled shipping modules
-require_once(DIR_WS_CLASSES . 'shipping.php');
+require_once DIR_WS_CLASSES . 'shipping.php';
 $shipping_modules = new shipping();
 
 $free_shipping = false;
-require_once(DIR_WS_MODULES . 'order_total/ot_shipping.php');
-include_once(DIR_WS_LANGUAGES . $_SESSION['language'] . '/modules/order_total/ot_shipping.php');
+require_once DIR_WS_MODULES . 'order_total/ot_shipping.php';
+include_once DIR_WS_LANGUAGES . $_SESSION['language'] . '/modules/order_total/ot_shipping.php';
 $ot_shipping = new ot_shipping();
 $ot_shipping->process();
 
@@ -134,11 +138,11 @@ if (defined('MODULE_CHECKOUT_EXPRESS_STATUS') && MODULE_CHECKOUT_EXPRESS_STATUS 
                 unset($express['checkout_shipping']);
             } elseif ($free_shipping === false && $express['checkout_shipping'] == 'cheapest_cheapest') {
               // get all available shipping quotes
-                $quotes = $shipping_modules->quote();
-                $cheapest = $shipping_modules->cheapest();
+                $quotes                       = $shipping_modules->quote();
+                $cheapest                     = $shipping_modules->cheapest();
                 $express['checkout_shipping'] = $cheapest['id'];
             }
-            $_POST['action'] = 'process';
+            $_POST['action']   = 'process';
             $_POST['shipping'] = (($free_shipping === true) ? 'free_free' : $express['checkout_shipping']);
         }
     }
@@ -147,7 +151,7 @@ if (defined('MODULE_CHECKOUT_EXPRESS_STATUS') && MODULE_CHECKOUT_EXPRESS_STATUS 
 // process the selected shipping method
 if (isset($_POST['action']) && ($_POST['action'] == 'process')) {
     $redirect_link = xtc_href_link(FILENAME_CHECKOUT_PAYMENT, xtc_get_all_get_params(), 'SSL');
-    require(DIR_WS_INCLUDES . 'shipping_action.php');
+    require DIR_WS_INCLUDES . 'shipping_action.php';
 }
 
 // get all available shipping quotes
@@ -162,12 +166,12 @@ if ((!isset($_SESSION['shipping']) && CHECK_CHEAPEST_SHIPPING_MODUL == 'true') |
 }
 
 // include boxes
-require(DIR_FS_CATALOG . 'templates/' . CURRENT_TEMPLATE . '/source/boxes.php');
+require DIR_FS_CATALOG . 'templates/' . CURRENT_TEMPLATE . '/source/boxes.php';
 
 $breadcrumb->add(NAVBAR_TITLE_1_CHECKOUT_SHIPPING, xtc_href_link(FILENAME_CHECKOUT_SHIPPING, '', 'SSL'));
 $breadcrumb->add(NAVBAR_TITLE_2_CHECKOUT_SHIPPING, xtc_href_link(FILENAME_CHECKOUT_SHIPPING, '', 'SSL'));
 
-require(DIR_WS_INCLUDES . 'header.php');
+require DIR_WS_INCLUDES . 'header.php';
 
 $smarty->assign('FORM_ACTION', xtc_draw_form('checkout_address', xtc_href_link(FILENAME_CHECKOUT_SHIPPING, xtc_get_all_get_params(), 'SSL'), 'post', 'onSubmit="return check_form();"') . xtc_draw_hidden_field('action', 'process'));
 $smarty->assign('ADDRESS_LABEL', xtc_address_label($_SESSION['customer_id'], $_SESSION['sendto'], true, ' ', '<br />'));
@@ -202,14 +206,16 @@ if (SHOW_SELFPICKUP_FREE == 'true') {
 }
 
 if (isset($_SESSION['cc_id'])) {
-    $coupon_query = xtc_db_query("SELECT *
+    $coupon_query = xtc_db_query(
+        "SELECT *
                                   FROM " . TABLE_COUPONS . "
                                  WHERE coupon_id = '" . (int)$_SESSION['cc_id'] . "'
                                    AND coupon_active = 'Y'
                                    AND (restrict_to_customers = ''
                                         OR restrict_to_customers IS NULL
                                         OR FIND_IN_SET ('" . (int)$_SESSION['customers_status']['customers_status_id'] . "', restrict_to_customers)
-                                        )");
+                                        )"
+    );
     if (xtc_db_num_rows($coupon_query) != 0) {
         $coupon_array = xtc_db_fetch_array($coupon_query);
         if ($coupon_array['coupon_type'] == 'S') {
@@ -227,7 +233,7 @@ if ($messageStack->size('checkout_shipping', 'success') > 0) {
 }
 
 // build shipping block
-require(DIR_WS_INCLUDES . 'shipping_block.php');
+require DIR_WS_INCLUDES . 'shipping_block.php';
 
 $smarty->assign('language', $_SESSION['language']);
 $smarty->assign('SHIPPING_BLOCK', $shipping_block);
@@ -239,4 +245,4 @@ if (!defined('RM')) {
 }
 $smarty->display(CURRENT_TEMPLATE . '/index.html');
 
-include('includes/application_bottom.php');
+include 'includes/application_bottom.php';
