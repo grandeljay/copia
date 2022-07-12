f589e8e22923e27c4462c3c7788e975bbcb1afe4
grandeljay <github.jay@grandel.anonaddy.me>
2022-07-12 08:57:22 +0200

    Update address_book.php

--- address_book.php
+++ address_book.php
@@ -17,22 +17,22 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
-include('includes/application_top.php');
+include 'includes/application_top.php';
 
 // create smarty elements
 $smarty = new Smarty();
 
 // include needed functions
-require_once(DIR_FS_INC . 'xtc_address_label.inc.php');
-require_once(DIR_FS_INC . 'xtc_get_country_name.inc.php');
-require_once(DIR_FS_INC . 'xtc_count_customer_address_book_entries.inc.php');
+require_once DIR_FS_INC . 'xtc_address_label.inc.php';
+require_once DIR_FS_INC . 'xtc_get_country_name.inc.php';
+require_once DIR_FS_INC . 'xtc_count_customer_address_book_entries.inc.php';
 
 if (!isset($_SESSION['customer_id'])) {
     xtc_redirect(xtc_href_link(FILENAME_LOGIN, '', 'SSL'));
 } elseif (
-    isset($_SESSION['customer_id'])
-          && $_SESSION['customers_status']['customers_status_id'] == DEFAULT_CUSTOMERS_STATUS_ID_GUEST
-          && GUEST_ACCOUNT_EDIT != 'true'
+       isset($_SESSION['customer_id'])
+    && DEFAULT_CUSTOMERS_STATUS_ID_GUEST == $_SESSION['customers_status']['customers_status_id']
+    && GUEST_ACCOUNT_EDIT != 'true'
 ) {
     xtc_redirect(xtc_href_link(FILENAME_DEFAULT, '', 'SSL'));
 }
@@ -46,7 +46,7 @@ unset($_SESSION['delivery_zone']);
 unset($_SESSION['billing_zone']);
 
 // include boxes
-require(DIR_FS_CATALOG . 'templates/' . CURRENT_TEMPLATE . '/source/boxes.php');
+require DIR_FS_CATALOG . 'templates/' . CURRENT_TEMPLATE . '/source/boxes.php';
 
 if ($messageStack->size('addressbook') > 0) {
     $smarty->assign('error', $messageStack->output('addressbook'));
@@ -58,36 +58,42 @@ if ($messageStack->size('addressbook', 'success') > 0) {
 
 $smarty->assign('ADDRESS_DEFAULT', xtc_address_label($_SESSION['customer_id'], $_SESSION['customer_default_address_id'], true, ' ', '<br />'));
 
-$addresses_data = array ();
-$addresses_query = xtc_db_query("SELECT address_book_id,
-                                        entry_firstname as firstname,
-                                        entry_lastname as lastname,
-                                        entry_company as company,
-                                        entry_street_address as street_address,
-                                        entry_suburb as suburb,
-                                        entry_city as city,
-                                        entry_postcode as postcode,
-                                        entry_state as state,
-                                        entry_zone_id as zone_id,
-                                        entry_country_id as country_id
-                                   FROM " . TABLE_ADDRESS_BOOK . "
-                                  WHERE customers_id = '" . (int) $_SESSION['customer_id'] . "'
-                               ORDER BY firstname, lastname");
+$addresses_data  = array ();
+$addresses_query = xtc_db_query(
+    "SELECT address_book_id,
+            entry_firstname      AS firstname,
+            entry_lastname       AS lastname,
+            entry_company        AS company,
+            entry_street_address AS street_address,
+            entry_suburb         AS suburb,
+            entry_city           AS city,
+            entry_postcode       AS postcode,
+            entry_state          AS state,
+            entry_zone_id        AS zone_id,
+            entry_country_id     AS country_id
+       FROM " . TABLE_ADDRESS_BOOK . "
+      WHERE customers_id = '" . (int) $_SESSION['customer_id'] . "'
+      ORDER BY firstname, lastname"
+);
+
 while ($addresses = xtc_db_fetch_array($addresses_query)) {
     $format_id = xtc_get_address_format_id($addresses['country_id']);
+
     if ($addresses['address_book_id'] == $_SESSION['customer_default_address_id']) {
         $primary = 1;
     } else {
         $primary = 0;
     }
-    $addresses_data[] = array ('NAME' => $addresses['firstname'] . ' ' . $addresses['lastname'],
-                             'BUTTON_EDIT' => '<a href="' . xtc_href_link(FILENAME_ADDRESS_BOOK_PROCESS, 'edit=' . $addresses['address_book_id'], 'SSL') . '">' . xtc_image_button('small_edit.gif', SMALL_IMAGE_BUTTON_EDIT) . '</a>',
-                             'BUTTON_DELETE' => '<a href="' . xtc_href_link(FILENAME_ADDRESS_BOOK_PROCESS, 'delete=' . $addresses['address_book_id'], 'SSL') . '">' . xtc_image_button('small_delete.gif', SMALL_IMAGE_BUTTON_DELETE) . '</a>',
-                             'ADDRESS' => xtc_address_format($format_id, $addresses, true, ' ', '<br />'),
-                             'PRIMARY' => $primary);
+
+    $addresses_data[] = array(
+        'NAME'          => $addresses['firstname'] . ' ' . $addresses['lastname'],
+        'BUTTON_EDIT'   => '<a href="' . xtc_href_link(FILENAME_ADDRESS_BOOK_PROCESS, 'edit=' . $addresses['address_book_id'], 'SSL') . '">' . xtc_image_button('small_edit.gif', SMALL_IMAGE_BUTTON_EDIT) . '</a>',
+        'BUTTON_DELETE' => '<a href="' . xtc_href_link(FILENAME_ADDRESS_BOOK_PROCESS, 'delete=' . $addresses['address_book_id'], 'SSL') . '">' . xtc_image_button('small_delete.gif', SMALL_IMAGE_BUTTON_DELETE) . '</a>',
+        'ADDRESS'       => xtc_address_format($format_id, $addresses, true, ' ', '<br />'),
+        'PRIMARY'       => $primary
+    );
 }
 $smarty->assign('addresses_data', $addresses_data);
-
 $smarty->assign('BUTTON_BACK', '<a href="' . xtc_href_link(FILENAME_ACCOUNT, '', 'SSL') . '">' . xtc_image_button('button_back.gif', IMAGE_BUTTON_BACK) . '</a>');
 
 if (xtc_count_customer_address_book_entries() < MAX_ADDRESS_BOOK_ENTRIES) {
@@ -99,17 +105,20 @@ $smarty->assign('ADDRESS_COUNT', sprintf(TEXT_MAXIMUM_ENTRIES, MAX_ADDRESS_BOOK_
 $breadcrumb->add(NAVBAR_TITLE_1_ADDRESS_BOOK, xtc_href_link(FILENAME_ACCOUNT, '', 'SSL'));
 $breadcrumb->add(NAVBAR_TITLE_2_ADDRESS_BOOK, xtc_href_link(FILENAME_ADDRESS_BOOK, '', 'SSL'));
 
-require(DIR_WS_INCLUDES . 'header.php');
+require DIR_WS_INCLUDES . 'header.php';
 
 $smarty->assign('language', $_SESSION['language']);
 $smarty->caching = 0;
-$main_content = $smarty->fetch(CURRENT_TEMPLATE . '/module/address_book.html');
+$main_content    = $smarty->fetch(CURRENT_TEMPLATE . '/module/address_book.html');
 
 $smarty->assign('language', $_SESSION['language']);
 $smarty->assign('main_content', $main_content);
 $smarty->caching = 0;
+
 if (!defined('RM')) {
     $smarty->load_filter('output', 'note');
 }
+
 $smarty->display(CURRENT_TEMPLATE . '/index.html');
-include('includes/application_bottom.php');
+
+include 'includes/application_bottom.php';
