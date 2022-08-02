17ca23c6942d702670568697447f947f809b287a
grandeljay <github.jay@grandel.anonaddy.me>
2022-08-02 09:08:19 +0200

    Update specials.php

--- specials.php
+++ specials.php
@@ -1,8 +1,21 @@
 <?php
 
-/**
- * specials.php
- */
+/* -----------------------------------------------------------------------------------------
+   $Id: specials.php 11989 2019-07-23 06:41:02Z GTB $
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   based on:
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(specials.php,v 1.47 2003/05/27); www.oscommerce.com
+   (c) 2003 nextcommerce (specials.php,v 1.12 2003/08/17); www.nextcommerce.org
+   (c) 2006 XT-Commerce (specials.php 1292 2005-10-07)
+
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
 
 include 'includes/application_top.php';
 
@@ -11,7 +24,7 @@ $smarty = new Smarty();
 // include boxes
 require DIR_FS_CATALOG . 'templates/' . CURRENT_TEMPLATE . '/source/boxes.php';
 
-if ($language_not_found === true) {
+if (true === $language_not_found) {
     $site_error = TEXT_SITE_NOT_FOUND;
     include DIR_WS_MODULES . FILENAME_ERROR_HANDLER;
 } else {
